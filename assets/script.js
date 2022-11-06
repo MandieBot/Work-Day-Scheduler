eb554e8d989ef454ba8b74929ee246cda39fd081
MandieBot <amandabotinelly@gmail.com>
2022-11-06 15:15:32 -0700

    Declaring Variables

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,18 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+var currentDate = document.getElementById("currentDay");
+
+// currentDate = moment().format("MMM Do YY");
+
+var savedEvents = JSON.parse(localStorage.getItem(""));
+
+var saveButton1 = document.getElementById("saveOne");
+
+saveButton1.addEventListener("click", function () {});
+
+$(function init() {
+  currentDate.textContent = moment().format("MMM Do YY");
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
